5f612ddbeed6ace5903170496398856c91f56d33
Chandra307 <chandra19151425@gmail.com>
2023-09-04 22:15:21 +0530

    serving views

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,16 +1,18 @@
 const express = require('express');
 
+const path = require('path');
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send(`<form action="/admin/product" method="POST">
-    <input type="text" name="product">
-    <input type="text" name="size">
-    <button type="submit">Add Product</button></form>`);
+    res.sendFile(path.join(rootDir,'views','admin.html'))
 })
+
 router.post('/product', (req, res, next) => {
     console.log(req.body);
-    res.redirect('/shop/');
+    res.redirect('/admin/add-product');
 })
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 const adminRoute = require('./Routes/admin.js');
 const shopRoute = require('./Routes/shop.js');
+const contactRoute = require('./Routes/contact.js');
+
 
 const bodyparser = require('body-parser');
 
@@ -11,9 +15,17 @@ app.use(bodyparser.urlencoded({extended: false}));
 
 app.use('/shop',shopRoute);
 app.use('/admin',adminRoute);
+app.use(contactRoute);
 
-app.use( (req, res, next) => {
-    res.status(404).send(`<h1>Page not found</h1>`)
+app.use(express.static(path.join(__dirname,'public')));
+
+app.post('/success',(req, res, next) => {
+    console.log(req.body);
+    res.send(`<h1>Form filled successfully</h1>`)
 })
 
-app.listen(3000);
+app.use( (req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views','notFound.html'))
+})
+
+app.listen(4000);
